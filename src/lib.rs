d1feced323dec3cb45b31bf01a73e9d99dbf9e9f
mrdkprj <you@example.com>
2024-09-27 19:26:18 +0900

    done

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,9 @@
-#![allow(unused_imports)]
 use async_std::sync::Mutex;
-use gdkx11::{
-    ffi::{gdk_x11_get_default_xdisplay, gdk_x11_lookup_xdisplay, gdk_x11_window_foreign_new_for_display, GdkX11Display, GdkX11Window},
-    X11Display, X11DisplayManager, X11Window,
-};
+use gdkx11::ffi::{gdk_x11_get_default_xdisplay, gdk_x11_lookup_xdisplay, gdk_x11_window_foreign_new_for_display};
 use gtk::{
-    ffi::{gtk_widget_new, GtkWindow},
-    gdk::{ffi::gdk_init, init},
-    glib::{self, ffi::g_idle_add, subclass::types::FromObject, translate::FromGlibPtrNone, ObjectExt},
-    prelude::{ContainerExt, WidgetExt, WindowExtManual},
-    ApplicationWindow, Widget,
-};
-use gtk::{
-    gdk::{ffi::GdkWindow, Window},
-    glib::{
-        idle_add,
-        translate::{FromGlibPtrFull, ToGlibPtr},
-        Cast,
-    },
+    gdk::Window,
+    glib::{self, translate::FromGlibPtrFull},
+    prelude::WidgetExt,
 };
 use neon::{
     handle::Handle,
@@ -92,26 +78,6 @@ pub fn build_from_template_with_config(mut cx: FunctionContext) -> JsResult<JsNu
     Ok(id)
 }
 
-// #[allow(dead_code)]
-// unsafe extern "C" fn b(data: *mut std::ffi::c_void) -> i32 {
-//     let data: &mut Data = unsafe { &mut *(data as *mut Data) };
-
-//     init();
-//     println!("parent:{:?}", data.parent);
-
-//     let mut builder = MenuBuilder::new_from_config(data.parent, data.config.clone());
-//     println!("parent2");
-//     build_menu(&mut builder, &data.items);
-//     let menu = builder.build().unwrap();
-
-//     let mut map = MENU_MAP.try_lock().unwrap();
-
-//     let inner = menu.gtk_menu_handle;
-//     (*map).insert(inner as i32, menu);
-
-//     0
-// }
-
 fn build(cx: &mut FunctionContext, parent: f64, templates: Vec<Handle<JsValue>>, config: Config) -> isize {
     let items: Vec<ElectronMenuItem> = templates
         .into_iter()
@@ -123,91 +89,26 @@ fn build(cx: &mut FunctionContext, parent: f64, templates: Vec<Handle<JsValue>>,
 
     #[cfg(target_os = "linux")]
     {
-        println!("linux:{:?}", parent);
-        // let data = Data {
-        //     parent: parent as isize,
-        //     config: config.clone(),
-        //     items: items.clone(),
-        // };
-        // let state_ptr: *mut std::ffi::c_void = &mut data as *mut _ as *mut std::ffi::c_void;
-        // let x = Box::into_raw(Box::new(data));
-        // idle_add(move || {
-        println!("parent:{:?}", parent);
-        // let d = gtk::gdk::Display::default().unwrap();
-        // gdkx11::X11Display::ff
-        // let display: *mut gdkx11::ffi::GdkX11Display = d.to_glib_none().0;
-        // let xwin: *mut GdkWindow = gdkx11::X11Window::foreign_new_for_display(display, data.parent as u64).to_glib_full().0;
-        // let _window = unsafe { gtk::gdk::Window::from_glib_full(xwin) };
-        // let _window: ApplicationWindow = unsafe { ApplicationWindow::from_glib_none(data.parent as *mut GtkApplicationWindow) };
-        // println!("ApplicationWindow");
+        let _ = gtk::init();
+        let display = unsafe { gdk_x11_get_default_xdisplay() };
 
-        if gtk::init().is_ok() {
-            println!("init");
-            // let gdk_window: X11Window = unsafe { X11Window::from_glib_full(parent as usize as *mut GdkX11Window) };
-            // let gdk_window: Window = unsafe { Window::from_glib_full(parent as usize as *mut GdkWindow) };
+        let x11_display = unsafe { gdk_x11_lookup_xdisplay(display) };
+        let gdkwindow = unsafe { gdk_x11_window_foreign_new_for_display(x11_display, parent as u64) };
+        let gdk_window: Window = unsafe { Window::from_glib_full(gdkwindow) };
+        let window = gtk::Window::new(gtk::WindowType::Toplevel);
+        window.connect_realize(glib::clone!(@weak gdk_window as wd => move |w| w.set_window(wd)));
+        window.set_has_window(true);
+        window.realize();
 
-            // println!("gdk_window:{:?}", gdk_window);
-            // let window: *mut GdkX11Window = unsafe { X11Window::from_glib_none(parent as isize) };
-            // let x = unsafe { gdk_x11_window_get_xid(window) };
-            // let x1 = Box::into_raw(Box::new(x));
-            let x = unsafe { gdk_x11_get_default_xdisplay() };
+        let mut builder = MenuBuilder::new_for_window_from_config(&window, config);
+        build_menu(&mut builder, &items);
+        let menu = builder.build().unwrap();
+        let mut map = MENU_MAP.try_lock().unwrap();
 
-            let gdkd = unsafe { gdk_x11_lookup_xdisplay(x) };
-            let w = unsafe { gdk_x11_window_foreign_new_for_display(gdkd, parent as u64) };
-            let gdk_window: Window = unsafe { Window::from_glib_full(w) };
-            println!("window:{:?}", gdk_window);
-            let window = gtk::Window::new(gtk::WindowType::Toplevel);
-            window.connect_realize(glib::clone!(@weak gdk_window as wd => move |w| w.set_window(wd)));
-            window.set_has_window(true);
-            window.realize();
-            window.reset_style();
+        let inner = menu.gtk_menu_handle;
+        (*map).insert(inner as i32, menu);
 
-            // let xwin: X11Window = gdk_window.dynamic_cast().unwrap();
-
-            // println!("{:?}", xwin.xid());
-
-            // let mut app: Option<Widget> = None;
-            // println!("list_toplevels");
-            // for x in gtk::Window::list_toplevels() {
-            //     if let Some(win) = x.window() {
-            //         println!("win:{:?}", win);
-
-            //         let xwin: X11Window = win.dynamic_cast().unwrap();
-
-            //         println!("{:?}", xwin.xid());
-            //         // if win == gdk_window {
-            //         //     println!("oik");
-            //         // }
-            //         app = Some(x);
-            //     }
-            // }
-
-            // let w: gtk::Window = app.unwrap().dynamic_cast().unwrap();
-
-            // println!("{:?}", w);
-            // println!("{:?}", config.color.dark.background_color);
-            // let r = wcpopup::config::rgba_from_hex(config.color.dark.background_color);
-            // println!("{:?}", r);
-            // let h = wcpopup::config::hex_from_rgba(r.r, r.g, r.b, r.a);
-            // println!("{:?}", h);
-
-            println!("parent2:{:?}", config.theme);
-            let mut builder = MenuBuilder::new_for_window_from_config(&window, config);
-
-            build_menu(&mut builder, &items);
-            println!("parent3");
-            let menu = builder.build().unwrap();
-            println!("{:?}", menu);
-            let mut map = MENU_MAP.try_lock().unwrap();
-
-            let inner = menu.gtk_menu_handle;
-            (*map).insert(inner as i32, menu);
-            return inner;
-        }
-        // gtk::glib::ControlFlow::Continue
-        // });
-        // unsafe { g_idle_add(Some(b), x as *mut std::ffi::c_void) };
-        0
+        inner
     }
 
     #[cfg(target_os = "windows")]
@@ -299,11 +200,9 @@ pub fn popup(mut cx: FunctionContext) -> JsResult<JsPromise> {
     });
     #[cfg(target_os = "linux")]
     gtk::glib::spawn_future_local(async move {
-        println!("enter");
         let map = MENU_MAP.lock().await;
         let menu = map.get(&(hwnd as i32)).unwrap();
         let x = menu.popup_at_async(x as i32, y as i32).await;
-        println!("returned");
         deferred.settle_with(&channel, |mut cx| match x {
             Some(data) => from_menu_item(&mut cx, &data),
             None => Ok(cx.empty_object()),
